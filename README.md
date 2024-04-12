a67354ec96552b2d559a8f0acc331fe3746b0f78
stammin001 <stammin@gmail.com>
2024-04-12 07:18:15 -0500

    remove all files

--- README.md
+++
@@ -1,5 +0,0 @@
-## HR-Agent
-
-This is an experimental app for HR-Agent. It contains basic Q&A features.
-
-
--- app/main/api/chats/route_temp.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(req: NextRequest) {
-    console.log("Hello, world!");
-
-    return NextResponse.json({"message": "Hello, world!"});
-}
